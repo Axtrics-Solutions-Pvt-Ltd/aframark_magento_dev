e86d0ddfa472da29e5e4efdc8a81f80786c88faf
shintu singh <61272772+shintusingh@users.noreply.github.com>
2020-05-25 18:01:09 +0530

    Update Category.php

--- Block/Category.php
+++ Block/Category.php
@@ -1,7 +1,13 @@
 <?php
+/**
+ * Copyright © 2020 Aframark. All rights reserved.
+ */
 namespace Axtrics\Aframark\Block;
 class Category extends \Magento\Framework\View\Element\Template
 {
+	/**
+     * @var \Axtrics\Aframark\Model\AframarkManagement
+     */
 	protected $_aframodel;
 	public function __construct(\Magento\Framework\View\Element\Template\Context $context,
 		\Axtrics\Aframark\Model\AframarkManagement $aframodel
@@ -11,6 +17,10 @@ class Category extends \Magento\Framework\View\Element\Template
 		parent::__construct($context);
 	}
 
+	/**
+	 * Function for getting aframark table data
+	 * @return object
+	 */
 	public function getAfraModel()
 	{
 		return $this->_aframodel;
