0c1508147bf58f209a74c11183485a153984c652
shintu singh <61272772+shintusingh@users.noreply.github.com>
2020-05-29 17:29:58 +0530

    Create Questiontab.php

---
+++ Block/Questiontab.php
@@ -0,0 +1,33 @@
+<?php
+namespace Axtrics\Aframark\Block;
+class Questiontab extends \Magento\Framework\View\Element\Template
+{
+	/**
+     * @var registry
+     */
+	protected $_registry;
+
+	/**
+     * @var aframark
+     */
+	protected $_aframark;
+
+	public function __construct(\Magento\Framework\View\Element\Template\Context $context,
+		\Magento\Framework\Registry $registry,
+		\Axtrics\Aframark\Model\Aframark $aframark)
+	{
+		$this->_registry = $registry;
+		$this->_aframark = $aframark;
+
+		parent::__construct($context);
+	}
+
+	 public function getCurrentProduct()
+    {        
+        return $this->_registry->registry('current_product');
+    } 
+    public function getAfraModel()
+    {
+    	return $this->_aframark;
+    } 
+}
