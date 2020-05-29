eddf092a9f18544fd2384a426b14c592c15f4abe
shintu singh <61272772+shintusingh@users.noreply.github.com>
2020-05-29 14:49:23 +0530

    Configurations delete after uninstall

--- Setup/Uninstall.php
+++ Setup/Uninstall.php
@@ -1,9 +1,12 @@
 <?php
 namespace Axtrics\Aframark\Setup;
 
-use Magento\Framework\Setup\UninstallInterface;
-use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Model\AbstractModel;
 use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Config\Model\ResourceModel\Config\Data;
+use Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory;
 
 class Uninstall implements UninstallInterface
 {
@@ -14,13 +17,48 @@ class Uninstall implements UninstallInterface
      * @param ModuleContextInterface $context
      * @return void
      */
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+    /**
+     * @var Data
+     */
+    protected $configResource;
+
+    /**
+     * @param CollectionFactory $collectionFactory
+     * @param Data $configResource
+     */
+    public function __construct(
+        CollectionFactory $collectionFactory,
+        Data $configResource
+    )
+    {
+        $this->collectionFactory = $collectionFactory;
+        $this->configResource    = $configResource;
+    }
     public function uninstall(
         SchemaSetupInterface $setup,
         ModuleContextInterface $context
     ) {
         $setup->startSetup();
-        $connection = $setup->getConnection();
-        $connection->dropTable($connection->getTableName('aframark_settings'));
+        if ($setup->tableExists('aframark_settings')) {
+            $setup->getConnection()->dropTable('aframark_settings');
+        }
+        $collection = $this->collectionFactory->create()
+            ->addPathFilter('Axtrics_Aframark_config');
+        foreach ($collection as $config) {
+            $this->deleteConfig($config);
+        }
         $setup->endSetup();
+    }
+     /**
+     * @param AbstractModel $config
+     * @throws \Exception
+     */
+    protected function deleteConfig(AbstractModel $config)
+    {
+        $this->configResource->delete($config);
     }
 }
