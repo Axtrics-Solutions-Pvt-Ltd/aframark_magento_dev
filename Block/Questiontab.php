431a4ade6f289615831455e31808a5cbb47719b5
shintu singh <61272772+shintusingh@users.noreply.github.com>
2020-06-11 09:36:36 +0530

    Added code for ask a question

--- Block/Questiontab.php
+++ Block/Questiontab.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * Copyright © 2020 Aframark . All rights reserved.
+ */
 namespace Axtrics\Aframark\Block;
 class Questiontab extends \Magento\Framework\View\Element\Template
 {
@@ -11,23 +14,53 @@ class Questiontab extends \Magento\Framework\View\Element\Template
      * @var aframark
      */
 	protected $_aframark;
+	/**
+	* Constructor.
+	*
+	* @param Magento\Framework\HTTP\Client\Curl $curl
+	*/
+	protected $_storeManager;
 
 	public function __construct(\Magento\Framework\View\Element\Template\Context $context,
 		\Magento\Framework\Registry $registry,
-		\Axtrics\Aframark\Model\Aframark $aframark)
+		\Axtrics\Aframark\Model\Aframark $aframark,
+		\Magento\Framework\HTTP\Client\Curl $curl,
+		\Magento\Store\Model\StoreManagerInterface $storeManager)
 	{
 		$this->_registry = $registry;
 		$this->_aframark = $aframark;
-
+		$this->curl = $curl;
+		$this->_storeManager = $storeManager;
 		parent::__construct($context);
 	}
 
-	 public function getCurrentProduct()
+	/**
+	 * Function for getting current product
+	 * @return object
+	 */
+	public function getCurrentProduct()
     {        
         return $this->_registry->registry('current_product');
-    } 
+    }
+
+    /**
+	 * Function for getting aframark model
+	 * @return object
+	 */ 
     public function getAfraModel()
     {
     	return $this->_aframark;
+    }
+
+    /**
+	 * Function for getting ask a question
+	 * @return json
+	 */
+    public function getAskQuestion($product,$appdata,$token)
+    {
+    	$url="https://sandbox.aframark.com/api/allqa/".$appdata['merchant_code']."/".$product->getSku()."/".$_SERVER['HTTP_HOST']."?api_token=".$token.$appdata['store_token']."";
+		$this->curl->get($url);
+		$result = $this->curl->getBody();
+		return $result;
     } 
 }
