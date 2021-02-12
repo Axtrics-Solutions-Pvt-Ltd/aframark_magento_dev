007cb1eadb017327630d37e2f7d4eebbb51c23f1
Shubham Kumar <66126731+shubhamaxtrics@users.noreply.github.com>
2021-02-12 14:09:56 +0530

    Cron Api Url updated

--- Model/Observer.php
+++ Model/Observer.php
@@ -7,7 +7,7 @@
 namespace Axtrics\Aframark\Model;
 use Magento\Framework\DB\Adapter\LockWaitException;
 /**
- * Simple Google Shopping observer
+ * Aframark Cron observer
  */
 class Observer 
 {
@@ -21,11 +21,19 @@ class Observer
      * @var objectmanager
      */
     protected $_objectManager;
-    protected $_pageFactory;
+     /**
+     * @var curl
+     */
+    protected $_curl;
+     /**
+     * @param Logger $logger
+     */
+    protected $helperblock;
     protected $coreDate = null;
     protected $_logger = null;
 
-	public function __construct(
+    public function __construct(
+        \Magento\Framework\HTTP\Client\Curl $curl,
         \Axtrics\Aframark\Logger\Logger $logger,
         \Magento\Framework\Stdlib\DateTime\DateTime $coreDate,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
@@ -33,9 +41,9 @@ class Observer
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
         \Axtrics\Aframark\Model\ResourceModel\Cron\CollectionFactory $cronCollection,
-		\Magento\Framework\View\Result\PageFactory $pageFactory)
-	{
-        $this->_pageFactory = $pageFactory;
+        \Axtrics\Aframark\Block\Data $helperBlock)
+    {
+        $this->_curl = $curl;
         $this->_logger = $logger;
         $this->coreDate = $coreDate;
         $this->_objectManager = $objectManager;
@@ -43,21 +51,21 @@ class Observer
         $this->productCollection = $productCollectionFactory;
         $this->cronCollection    = $cronCollection;
         $this->scopeConfig = $scopeConfig;
+        $this->helperblock = $helperBlock;
     }
 
     public function SyncProducts(\Magento\Cron\Model\Schedule $schedule)
     {
+     $this->_logger->notice("-------------------- CRON Started Running--------------------");
         try {
+            
+           
         if( $this->jobHasAlreadyBeenRun('aframark_cron_import')){
             $this->_logger->notice("-------------------- CRON Already Running --------------------");
             return;
            }
         $limit=50;
-        $model = $this->_objectManager->create('Axtrics\Aframark\Model\Aframark');
-        $app_data=$model->getCollection()->getFirstItem();
-        $app_data_update = $model->load($app_data['app_id']);
-
-        $this->_logger->notice("-------------------- CRON Startted --------------------");
+        $this->_logger->notice("-------------------- CRON Started --------------------");
         $log[] = "-------------------- CRON PROCESS --------------------";
     $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
    $Cronfeed= $this->scopeConfig->getValue('Axtrics_Aframark_config/cron_mapping_setting/aframark_cron', $storeScope);
@@ -71,11 +79,33 @@ class Observer
    if ($cronExpr != null && isset($cronExpr->days)) {
     foreach ($cronExpr->days as $d) {
         foreach ($cronExpr->hours as $h) {
+          
          $time = explode(':', $h);
          if (date('l', $cron['current']['gmtTime']) == $d && date('H', $cron['current']['gmtTime'])==$time[0]) {
-            
+            $model = $this->_objectManager->create('Axtrics\Aframark\Model\Aframark');
+
+            $app_data=$model->getCollection()->getFirstItem();
+            $app_data_update = $model->load($app_data['app_id']);
+
             $collection =  $this->productCollection->create();
             $collection->addAttributeToFilter('status',\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED);
+
+            $count= $collection->getSize();
+            $counttoken = substr(md5(microtime()),rand(0,26),5);
+            $countarray=array('count' => $count);
+            $urll=$this->helperblock->getProductCountCronUrl()."?api_token=".$counttoken.$app_data['store_token'];
+            $this->_curl->post($urll, $countarray);
+            $countresponse = $this->_curl->getBody();
+            $countarray=json_decode($countresponse);
+            $countarray=json_decode($countresponse);
+            if ($countarray == "Success") {
+                $this->_logger->info("Count Success".$countresponse);
+            }
+            else
+            {
+                $this->_logger->info("Count Error".$countresponse);
+            }
+           
             $collection->setCurPage(1)->setPageSize($limit)->load();
                     $numberOfPages = $collection->getLastPageNumber();
                     for ($i=1; $i<=$numberOfPages; $i++) {
@@ -116,7 +146,7 @@ class Observer
                             $image_url = $this->_storeManager->getStore()->getBaseUrl(\Magento\Framework\UrlInterface::URL_TYPE_MEDIA).'catalog/product'.$productData->getImage();
                             $cats=$productData->getCategoryIds();
                             $categorys=array();
-                            if(count($cats) ){
+                            if(!empty($cats) ){
             foreach ($cats as $cat) {
                  $_category =  $this->_objectManager->create('Magento\Catalog\Model\Category')->load($cat);
                  $categorys[]= $_category->getName();
@@ -126,23 +156,45 @@ class Observer
     }
                     $product_collections[]=array(
                             'id'=>$product['entity_id'],
-                            'title'=>$productData->getName(),
+                            'title'=>$productData->getName()."Updated again".$i,
                             'sku'=>$product['sku'],
                             'image'=>$image_url,
                             'category'=>$categorys,
-                            'mpn'=>$productData[$mpn],
-                            'upc'=>$productData[$upc],
-                            'ean'=>$productData[$ean],
-                            'isbn'=>$productData[$isbn],
+                            'mpn'=>!empty($productData[$mpn])?$productData[$mpn]:'',
+                            'upc'=>!empty($productData[$upc])?$productData[$upc]:'',
+                            'ean'=>!empty($productData[$ean])?$productData[$ean]:'',
+                            'isbn'=>!empty($productData[$isbn])?$priceroductData[$isbn]:'',
                             'price'=> $productData->getPrice(),
                             'url'=> $productData->getProductUrl(),
     
                        );
                     
                         }
-                        $this->_logger->log('600', print_r($product_collections, true));
+                        $token = substr(md5(microtime()),rand(0,26),5);
+                        $responsedata=array('products' => $product_collections);
+                        $url=$this->helperblock->getProductCronUrl()."?api_token=".$token.$app_data['store_token'];
+                        $this->_curl->post($url, $responsedata);
+                        $response = $this->_curl->getBody();
+                        $responsearray=json_decode($response, TRUE);
+                        $this->_logger->info("Product Cron Sucess".$response);
+                        if (!empty($responsearray)) {
+                        if ($responsearray[0]['status'] == "200") 
+                        {
+                        $this->_logger->info("Product Cron Sucess".$response);
+
+                        }
+                        else
+                        {
+                        $this->_logger->info("Product Cron Fail".$response);
+                        }
+                        }
+                        else
+                        {
+                        $this->_logger->info("Empty Response for sku".$product->getSku());
+                        }
+    
                     }
-            }  
+                }
             }
         }
     }
